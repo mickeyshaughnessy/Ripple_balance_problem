c80e4f10ac8bb211ca5b084ef64dbfc2dde48529
Michael Shaughnessy <michaelshaughnessy@Michaels-MacBook-Air.local>
2014-12-30 02:19:12 -0800

    full working version

--- get_balances.js
+++ get_balances.js
@@ -11,30 +11,12 @@ var remote = new Remote({
   servers: [ 'wss://s1.ripple.com:443' ]
 });
 
-function Account(number) {
-    this.number = number;
-    this.balance = 0;
-    this.IOUs = {};
-}
-
-//function getAccountBalance(account_number){
-    
-//function appendIOUs (html, IOUs, i) {
-//    var values = {}; 
-//    for (var line in IOUs) {
-//        if (line.currency in values){
-//            values[line.currency] += parseFloat(line.balance)
-//        }
-//        else{
-//            values[line.currency] = parseFloat(line.balance)
-//        }
-//    }
-//    for (var currency in values){
-//        html += 'Currency :' + currency + 'Total: ' + values[currency] + '<br>'  
-//    }
-//    ExtractData( i + 1 )
+//function Account(number) {
+//    this.number = number;
+//    this.balance = 0;
+//    this.IOUs = {};
 //}
- 
+
 remote.connect(function() {
   remote.requestServerInfo(function(err, info) {
     });
@@ -55,24 +37,54 @@ app.post('/', function(req, res){
     var accounts = AccountNumbers.split(',');
     var html = '';
     
-    function appendIOUs (html, IOUs, i) {
-        var values = {};
-        for (var line in IOUs) {
-            if (line.currency in values){
-                values[line.currency] += parseFloat(line.balance)
+    function appendIOUs (html, IOUs, i, finished) {
+//        console.log('IOUs: %j', IOUs);
+        var totals = {};
+//        console.log(IOUs); 
+//        console.log('first totals: %j', totals);
+        for (var j = 0; j < IOUs.length; j++) {
+//            console.log('intermediate totals: %j', totals);
+            var line = IOUs[j];
+//            console.log(line);
+//            console.log(line.currency);
+//            console.log('key % value %s', Object.keys(totals), );
+//            console.log(line.currency);
+//            console.log('target currency %s, available currencies %s', line.currency, Object.keys(totals));
+//            console.log(Object.keys(totals));
+            if (Object.keys(totals).indexOf(line.currency) > 0 ) {
+//                console.log('current totals: %s', totals[line.currency]);
+//                console.log('amount to add %s, cast %s', line.balance, parseFloat(line.balance));
+                totals[line.currency] += parseFloat(line.balance);
+//                console.log('next values: %s', values[line.currency]);
+//                console.log(values);
             }
             else{
-                values[line.currency] = parseFloat(line.balance)
+//                console.log('first values: %s for %s', values[line.currency], line);
+                totals[line.currency] = parseFloat(line.balance);
             }
         }
-        for (var currency in values){
-            html += 'Currency :' + currency + 'Total: ' + values[currency] + '<br>'
+        console.log('final totals: %j', totals);
+        for (var currency in totals){
+            html += 'Currency :' + currency + 'Total: ' + totals[currency] + '<br>';
         }
-        ExtractData( i + 1 )
+        console.log('hi %s',html);
+        finished += 1;
+        if (finished == accounts.length){
+            emitter.emit('done', res, html);
+        } 
+        ExtractData(null, i + 1 , finished, html)
     }
  
-    function ExtractData(i) {
-        if( i < accounts.length ) {
+    //var send_html = function (err, html, res) {
+    //    res.send(html);
+    //};
+
+    var emitter = new (require('events').EventEmitter);
+    emitter.on('done', function (res, html) { res.send(html); });
+
+    function ExtractData(err, i, finished, html) {
+        if ( i < accounts.length ) {
+            console.log(i);
             html += 'Account: ' + accounts[i] + '<br>';
             var options = {
             account: accounts[i],
@@ -83,15 +95,20 @@ app.post('/', function(req, res){
                 html += 'Balance (XRP): ' + info["account_data"]["Balance"] + '<br>';
                 var requestIOU = remote.requestAccountLines(options, function(err, info) {
                     IOUs = info.lines;
-                    appendIOUs (html, IOUs, i)
+                    appendIOUs (html, IOUs, i, finished)
                 });
                 // insert get IOU code here and make it callback ExtractData(i+1)? 
             });
         }
-        else { res.send(html); }
+        else { 
+            console.log('hola %s',html)
+            //callback(null, html, res);
+        //    console.log(i);
+        //    //res.send(html);
+        }
     }
-
-    ExtractData(0)
+    var finished = 0;
+    ExtractData(null, 0, finished, html)
 }); 
 app.listen(8080);
 
@@ -114,3 +131,6 @@ for currency in values:
     html += 'Currency :' + currency + 'Total: ' + values[currency] + '<br>'
 
 */
+
+//  rUB4uZESnjSp2r7Uw1PnKSPWNMkTDCMXDJ,rUB4uZESnjSp2r7Uw1PnKSPWNMkTDCMXDJ
+// rBTC1BgnrukYh9gfE8uL5dqnPCfZXUxiho,rUB4uZESnjSp2r7Uw1PnKSPWNMkTDCMXDJ,r4ffgYrACcB82bt99VnqH4B9GEntEypTcp
